836366ed905e2a6a64fadecf501f90d5016c1359
Zuzim <zajo@ssct.edu.ph>
2022-12-04 21:46:05 +0800

    adding border when cursor to image

--- frontend/src/components/Image/UserAvatar.tsx
+++ frontend/src/components/Image/UserAvatar.tsx
@@ -6,19 +6,36 @@ type Props = {
     src: string;
     margin?: string;
     alt: string;
+    click?: (event: React.MouseEvent<HTMLDivElement>)=> void;
+    cursor?: string;
 }
 
 const UserAvatar = (props: Props)=> {
   return (
-    <Image {...props} />
+    <ImageContainer onClick={props.click} {...props}>
+    <Image cursor={props.cursor} src={props.src} alt={props.alt}  />
+    </ImageContainer>
   )
 }
 
 
-const Image = styled(Avatar)<Props>(({ width, margin, height })=> ({
-    width,
-    height,
-    margin,
+const ImageContainer = styled('div')<Props>(({ cursor, width, margin, height })=>({
+  width,
+  height,
+  margin,
+  cursor,
+
+
+}))
+
+
+const Image = styled(Avatar)<{cursor?:string}>(({cursor, theme})=>({
+    width: "100%",
+    height: "100%",
+
+  '&:hover': {
+    border: cursor && theme.palette.mode === "light" ? "2px solid var(--maintext-light)" : "2px solid var(--maintext-dark)",
+  }
 }))
 
 export default UserAvatar
