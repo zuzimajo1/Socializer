f49a486ae1ba643dbdae2ab35db4706beef3cd87
Zuzim <zajo@ssct.edu.ph>
2022-12-05 22:53:20 +0800

    change proptype

--- frontend/src/components/Form/ButtonSubmit.tsx
+++ frontend/src/components/Form/ButtonSubmit.tsx
@@ -1,19 +1,20 @@
-import { Button, styled } from '@mui/material';
-import LoadingButton from '@mui/lab/LoadingButton';
 import React from 'react'
+import { Button, styled } from '@mui/material';
+
+import LoadingButton from '@mui/lab/LoadingButton';
 
 type Props = {
   title: string;
   variant: "text" | "contained" | "outlined";
   padding?: string;
   Loading?: boolean;
-  Icon?: any;
+  Icon?: React.ElementType;
   click?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 const ButtonSubmit = ({ title, variant, padding, Loading, Icon, click }: Props) => {
   return (
-    <ButtonComponent onClick={click} loading={Loading} loadingPosition="start" startIcon={<Icon />} padding={padding} variant={variant}>{title}</ButtonComponent>
+    <ButtonComponent onClick={click} loading={Loading} loadingPosition="start" startIcon={Icon && <Icon />} padding={padding} variant={variant}>{title}</ButtonComponent>
   )
 }
 
