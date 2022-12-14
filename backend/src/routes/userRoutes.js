b1afc6de3e347b4eb5f6e89f6383c8e5b313aad1
Zuzim <zajo@ssct.edu.ph>
2022-12-14 13:49:17 +0800

    add router the new controller

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -4,6 +4,6 @@ var Authenticate = require("../middleware/jwt");
 
 router.post("/auth/register", AuthControllers.register);
 router.post("/auth/login", AuthControllers.login);
+router.get("/auth/entry", Authenticate.verifyToken, AuthControllers.entry);
 router.patch("/auth/changepassword", Authenticate.verifyToken , AuthControllers.changepassword);
-
 module.exports = router;
