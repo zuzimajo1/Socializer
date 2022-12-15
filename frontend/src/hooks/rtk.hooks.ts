a6fe57b4a168ada29b8f65e5d24e11f99602bbbd
Zuzim <zajo@ssct.edu.ph>
2022-12-15 21:48:00 +0800

    implementing rtk hooks

--- frontend/src/hooks/rtk.hooks.ts
+++ frontend/src/hooks/rtk.hooks.ts
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
+
 import type { RootState, AppDispatch } from "../features/store";
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
