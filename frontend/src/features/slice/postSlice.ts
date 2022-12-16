fd91075695a7f0349d64fa57ec93596549d5a639
Zuzim <zajo@ssct.edu.ph>
2022-12-17 01:32:08 +0800

    adding more case of created async thunks

--- frontend/src/features/slice/postSlice.ts
+++ frontend/src/features/slice/postSlice.ts
@@ -1,6 +1,6 @@
 import { ActionReducerMapBuilder, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { APIResponse, PostProps } from "../../utils/types";
-import { fetchAllPost } from "../asyncThunk";
+import { fetchAllPost, userDeletePost, userPost } from "../asyncThunk";
 
 
 export interface PostState{
@@ -30,20 +30,51 @@ const postSlice: any = createSlice({
     },
     extraReducers: (builder : ActionReducerMapBuilder<PostState>)=>{
         builder
-        .addCase(fetchAllPost.pending, (state: PostState)=>{
+          .addCase(fetchAllPost.pending, (state: PostState) => {
             state.isLoading = true;
             state.isSuccess = false;
-        })
-        .addCase(fetchAllPost.fulfilled, (state: PostState, action: PayloadAction<APIResponse<{}>>)=>{
-            const data = action.payload.data as PostProps[];
-            state.isLoading = false;
-            state.post = data;
-            state.isSuccess = true;
-        })
-        .addCase(fetchAllPost.rejected, (state: PostState)=>{
+          })
+          .addCase(
+            fetchAllPost.fulfilled,
+            (state: PostState, action: PayloadAction<APIResponse<{}>>) => {
+              const data = action.payload.data as PostProps[];
+              state.isLoading = false;
+              state.post = data;
+              state.isSuccess = true;
+            }
+          )
+          .addCase(fetchAllPost.rejected, (state: PostState) => {
             state.isSuccess = false;
             state.isLoading = false;
-        })
+          })
+          .addCase(userPost.pending, (state: PostState) => {
+            state.isLoading = false;
+            state.isSuccess = true;
+          })
+          .addCase(
+            userPost.fulfilled, (state: PostState, action: PayloadAction<APIResponse<{}>>) => {
+              const data = action.payload.data as PostProps;
+              state.post.unshift(data);
+              state.isSuccess = true;
+            }
+          )
+          .addCase(userPost.rejected, (state: PostState) => {
+            state.isSuccess = false;
+          })
+          .addCase(userDeletePost.pending, (state: PostState) => {
+            state.isLoading = false;
+            state.isSuccess = true;
+          })
+          .addCase(userDeletePost.fulfilled, (state: PostState, action: PayloadAction<APIResponse<{}>>)=>{
+            const data = action.payload.data as PostProps;
+            state.post = state.post.filter((items)=> items._id !== data._id);
+            state.isSuccess = true;
+          })
+          .addCase(userDeletePost.rejected, (state: PostState)=>{
+              state.isLoading = false;
+              state.isSuccess = true;
+          })
+        
     }
 
 })
