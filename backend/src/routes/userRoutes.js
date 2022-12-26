2d214e0154c7d7d995ce3439570dd956f0b80146
Zuzim <zajo@ssct.edu.ph>
2022-12-26 23:18:54 +0800

    add the change image router

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,9 +1,12 @@
 var router = require("express").Router();
+
 var AuthControllers = require("../controller/AuthController");
 var Authenticate = require("../middleware/jwt");
+const { upload } = require("../helpers/fileHelper");
 
 router.post("/auth/register", AuthControllers.register);
 router.post("/auth/login", AuthControllers.login);
 router.get("/auth/entry", Authenticate.verifyToken, AuthControllers.entry);
 router.patch("/auth/changepassword", Authenticate.verifyToken , AuthControllers.changepassword);
+router.patch("/auth/changeimage", Authenticate.verifyToken , upload.single("image"), AuthControllers.setImage );
 module.exports = router;
