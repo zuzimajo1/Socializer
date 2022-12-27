8b9ce0f087c8b88ea192bab382a2e7d706184019
Zuzim <zajo@ssct.edu.ph>
2022-12-27 21:29:42 +0800

    implement firebase

---
+++ frontend/src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB5RXtcK1DSsND_oV0LtyX74Nic8ionjMw",
+  authDomain: "bermudez-mobileapp.firebaseapp.com",
+  projectId: "bermudez-mobileapp",
+  storageBucket: "bermudez-mobileapp.appspot.com",
+  messagingSenderId: "1068319749652",
+  appId: "1:1068319749652:web:a02878905e93d587389901",
+};
+
+// Initialize Firebase
+export const firebaseapp = initializeApp(firebaseConfig);
